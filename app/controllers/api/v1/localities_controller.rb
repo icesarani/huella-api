96d33b5c04d9775e0a873cefa96afb1749286062
Ignacio Cesarani <icesarani@crunchloop.io>
2025-09-08 05:48:05 +0000

    fix: chusaku runs in localities index action

--- app/controllers/api/v1/localities_controller.rb
+++ app/controllers/api/v1/localities_controller.rb
@@ -3,6 +3,7 @@
 module Api
   module V1
     class LocalitiesController < BaseController
+      # @route GET /api/v1/localities (api_v1_localities)
       def index
         localities = Localities::SearchService.new.call
 
